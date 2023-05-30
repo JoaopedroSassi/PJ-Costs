9e05563d96c59a2e4fcfe3ae1d1e4b3710f63719
JoaopedroSassi <joaopedro10813@gmail.com>
2023-05-30 17:25:10 -0300

    Add app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <p>Costs</p>
-    </div>
+    <Router>
+      <ul>
+        <li>Home</li>
+        <li>Contato</li>
+
+      </ul>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/company">
+          <Company />
+        </Route>
+        <Route path="/contact">
+          <Contact />
+        </Route>
+        <Route path="/newproject">
+          <NewProject />
+        </Route>
+      </Switch>
+      <p>Footer</p>
+    </Router>
   );
 }
 
